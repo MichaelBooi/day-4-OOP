54e95d92339db5732658add450d4beb86e4e2e47
MichaelBooi <michaelbooi@hotmail.com>
2016-10-05 10:39:53 +0200

    choices

---
+++ dish.rb
@@ -0,0 +1,7 @@
+class Dish
+  def initialize(dish_name)
+    @name = dish_name
+  end
+end
+
+require './dish'
---
+++ menu.rb
@@ -0,0 +1,8 @@
+class Menu
+  def initialize
+    @menu << Dish.new("Margherita")
+    @menu << Dish.new("Napoletana")
+    @menu << Dish.new("Peperoni")
+  end
+
+end
