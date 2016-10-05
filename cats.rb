0aabeb30dc8f8a9fa3cdf69f61adb1045d3037cd
MichaelBooi <michaelbooi@hotmail.com>
2016-10-05 10:25:36 +0200

    choices

---
+++ cats.rb
@@ -0,0 +1,21 @@
+cat1 = {
+  :name => "Twinky",
+  :age => 13,
+  :fur => "black",
+  :amount_of_eyes => 3
+}
+
+cat2 = {
+  :name => "George",
+  :age => 17,
+  :fur => "black",
+  :amount_of_eyes => 20
+}
+
+class cat1
+  attr_accessor :name, :age, :fur, :amount_of_eyes
+end
+
+class cat2
+  attr_accessor :name, :age, :fur, :amount_of_eyes
+end
--- waiter.rb
+++ waiter.rb
@@ -2,4 +2,21 @@ class Waiter
   def greet_guest
     p "Good day, welcome to our pizzeria!"
   end
+  def serve_guest
+    p "What can I order for you?"
+    p "1. Would you like to order a pizza?"
+    p "2. If not, what are you even doing here?"
+    choice = gets.chomp.to_i
+    take_order(choice)
+  end
+  def take_order(order_number)
+    case order_number
+    when 1
+      p "Let me get the menu"
+    when 2
+     p  "Thank you for your visit"
+    else
+      p "I really don't understand"
+  end
+  end
 end
